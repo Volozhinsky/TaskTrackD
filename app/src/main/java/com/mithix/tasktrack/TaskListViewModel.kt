f63aebe469931f9374c15270abd46ba770346eac
Volozhinsky <vmmtorg@gmail.com>
2022-02-26 23:30:14 +0300

    Start git

--- app/src/main/java/com/mithix/tasktrack/TaskListViewModel.kt
+++ app/src/main/java/com/mithix/tasktrack/TaskListViewModel.kt
@@ -2,17 +2,20 @@ package com.mithix.tasktrack
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.api.services.tasks.Tasks
 import kotlinx.coroutines.launch
 
-class TaskListViewModel:ViewModel() {
+class TaskListViewModel(private val service: Tasks?):ViewModel() {
     private val taskRepository = TaskRepository.get()
     private val googleTaskRepository = GoogleTaskRepository.get()
     val taskListLiveData = taskRepository.getTasks()
-    val googleTaskListLiveData =googleTaskRepository.getTasks()
+    var googleTaskListLiveData =googleTaskRepository.getTasks(service)
 
 
     fun addTask(task: Task){
         taskRepository.addTask(task)
     }
-
+    fun loadGooglelist(serviceN: Tasks?){
+        googleTaskListLiveData = googleTaskRepository.getTasks(serviceN)
+    }
 }
